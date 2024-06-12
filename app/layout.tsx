894048ba1e53c108059429e174d410b406bfe8fd
Guilherme Cardoso <guilhermecardoso.info@gmail.com>
2024-06-11 22:06:41 -0300

    chore: add fonts in project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,22 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const myFont = localFont({
+  src: [
+    {
+      path: './fonts/HelveticaNeueThin.otf',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: './fonts/HelveticaNeueUltraLight.otf',
+      weight: '300',
+      style: 'normal'
+    }
+  ]
+})
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={myFont.className}>{children}</body>
     </html>
   );
 }
