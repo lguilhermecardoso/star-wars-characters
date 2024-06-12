119464497bc5662c237e71061bd57d17505a2d31
Guilherme Cardoso <guilhermecardoso.info@gmail.com>
2024-06-12 09:45:43 -0300

    feat: add header application component

--- app/_components/_header/index.tsx
+++ app/_components/_header/index.tsx
@@ -1,7 +1,11 @@
 export function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  )
+    <header className="m-auto max-w-[1320px] px-14 pt-20">
+      <h1 className="text-5xl font-normal">Star Wars Characters</h1>
+      <h2 className="mt-6 max-w-[934px] text-[22px] leading-8 text-[#666666]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </h2>
+    </header>
+  );
 }
