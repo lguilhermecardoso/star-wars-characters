5e478de21b324081500f2e4875d50e54d66d59a1
Guilherme Cardoso <guilhermecardoso.info@gmail.com>
2024-06-12 16:25:24 -0300

    fix: adjusments on filter component

--- app/_components/Filter.tsx
+++ app/_components/Filter.tsx
@@ -16,15 +16,18 @@ export default function Filter({ planets, onFilterChange }: FilterProps) {
   }, [selectedPlanet]);
 
   return (
-    <div className="m-auto max-w-[1320px] px-14 pt-10">
-      <label htmlFor="planet-filter" className="block text-[20px] font-normal">
-        Filter by Homeworld:
+    <div className="m-auto flex max-w-[1320px] items-center px-14 pt-10">
+      <label
+        htmlFor="planet-filter"
+        className="mt-2 block text-base font-normal"
+      >
+        Filter by:
       </label>
       <select
         id="planet-filter"
         value={selectedPlanet}
         onChange={(e) => setSelectedPlanet(e.target.value)}
-        className="mt-2 w-[200px] rounded border border-gray-950 px-4 py-2"
+        className="ml-3 mt-2 w-[200px] rounded border border-gray-950 px-4 py-2"
       >
         <option value="All">All</option>
         {planets.map((planet) => (
